2ee1b2088513dd4cf7bac85847e47452f6f88008
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2020-12-23 11:59:10 +0530

    Create 1.cpp

---
+++ C++/OOP/1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class A{
+	public:
+		A()
+		{
+			cout<<"Constructor of A"<<endl;
+		}
+		~A()
+		{
+			cout<<"Destructor of A"<<endl;
+		}
+};
+class B{
+	public:
+		B()
+		{
+			cout<<"Constructor of B"<<endl;
+		}
+		~B()
+		{
+			cout<<"Destructor of B"<<endl;
+		}
+};
+class C:public A,public B{
+	public:
+		C()
+		{
+			cout<<"Constructor of C"<<endl;
+		}
+		~C()
+		{
+			cout<<"Destructor of C"<<endl;
+		}
+};
+int main()
+{
+	C c;
+	return 0;
+}
