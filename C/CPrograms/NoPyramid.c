f0752ded71cc7d40adf72d951855f6fa34fe46d1
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2020-12-30 09:48:29 +0530

    Add files via upload

---
+++ C/CPrograms/NoPyramid.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{
+	int n,i,j,c=1;
+	printf("Enter the Number\n");
+	scanf("%d",&n);
+	c=n-1;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=c;j++)
+		{
+			printf(" ");
+		}
+		c++;
+		for(j=1;j<=2*(n-i)+1;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
