b6d0b7fca57ddb311fcd5d22f346095882deba8e
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2021-01-07 15:44:35 +0530

    Add files via upload

---
+++ Python/Guess.py
@@ -0,0 +1,24 @@
+import random
+
+value = random.randint(1, 10)
+count = 0
+guess = 0
+print('Guess a number between 1 and 10')
+
+while guess != value:
+    count += 1
+    guess = input(f'Enter guess #{count}: ')
+
+    if guess.isnumeric():
+        guess = int(guess)
+    else:
+        print('Numbers only, please!')
+        continue
+
+    if guess > value:
+        print('Your guess is too high, try again!')
+    elif guess < value:
+        print('Your guess is too low, try again!')
+
+else:
+    print(f'You guessed it in {count} tries!')
