d386ee334b8f20f409e6cdf55ae0b9ecbc0d883d
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2021-01-17 08:18:15 +0530

    Create Meteor.py

---
+++ Python/Projects/OverTheMoon/meteor-showers/Meteor.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import pandas as pd
+meteor_showers = pd.read_csv('data/meteorshowers.csv') 
+moon_phases = pd.read_csv('data/moonphases.csv')
+constellations = pd.read_csv('data/constellations.csv')
+cities = pd.read_csv('data/cities.csv')
