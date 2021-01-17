01355aced565a55fddfba8f5b1329e810ab7d2e3
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2021-01-17 08:19:07 +0530

    Update Meteor.py

--- Python/Projects/OverTheMoon/meteor-showers/Meteor.py
+++ Python/Projects/OverTheMoon/meteor-showers/Meteor.py
@@ -4,3 +4,10 @@ meteor_showers = pd.read_csv('data/meteorshowers.csv')
 moon_phases = pd.read_csv('data/moonphases.csv')
 constellations = pd.read_csv('data/constellations.csv')
 cities = pd.read_csv('data/cities.csv')
+change_meteor_shower = {'name':'Chang\'e','radiant':'Draco','bestmonth':'october','startmonth':'october','startday':1,'endmonth':'october','endday':31,'hemisphere':'northern','preferredhemisphere':'northern'}
+meteor_showers = meteor_showers.append(change_meteor_shower, ignore_index=True)
+draco_constellation = {'constellation':'Draco','bestmonth':'july','latitudestart':90,'latitudeend':-15,'besttime':2100,'hemisphere':'northern'}
+constellations = constellations.append(draco_constellation, ignore_index=True)
+meteor_showers.info()
+
+
