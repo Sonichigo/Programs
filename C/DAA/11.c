034f0abf938e06e81b135df63c9b64efd7bfe3f5
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2021-03-30 17:13:39 +0530

    Add files via upload

--- C/DAA/11.c
+++ C/DAA/11.c
@@ -1,21 +1,38 @@
-#include <iostream>
-#include <ctime>
-#include <cmath>
-using namespace std;
-
-int main ()
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+double t;
+int linear(int[],int,int);
+int main()
 {
-	float x,y;
-	clock_t time_req;
-
-	// Using pow function
-	time_req = clock();
-	for(int i=0; i<100000; i++)
+	int i,ar[50000],item,n,index;
+	printf("Enter size:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
 	{
-		y = log(pow(i,5));
+		ar[i]=rand()%n;
 	}
-	time_req = clock() - time_req;
-	cout << "Using pow function, it took " << (float)time_req/CLOCKS_PER_SEC << " seconds" << endl;
-
+	printf("Last element:%d",ar[n-1]);
+	printf("\nEnter element to be searched for:");
+	scanf("%d",&item);
+	t = clock();
+	index=linear(ar,n,item);
+	if(index==1)
+	printf("\nNumber not present\n");
+	else
+	printf("\nElement found at index:%d",index);
+	t = clock() - t;
+	double time_taken = ((double)t)/CLOCKS_PER_SEC;
+	printf("\nTime taken:%f\n", time_taken);
 	return 0;
+}
+int linear(int ar[],int size,int item)
+{
+	int i;
+	for(i=0;i<size;i++)
+	{
+		if(ar[i]==item)
+		return i;
+	}
+	return -1;
 }
