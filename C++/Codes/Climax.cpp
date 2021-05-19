df34a9bad90e80991753ca1894cc2f7d4f2036e2
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2021-05-19 23:21:56 +0530

    Create Climax.cpp

---
+++ C++/Codes/Climax.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n,m;
+  cin>>n>>m;
+  int ar[n][m];
+  int i,j,x,y;
+  for(i=0;i<n;i++){
+    for(j=0;j<m;j++){
+      cin>>ar[i][j];
+    }
+  }
+  cin>>x>>y;
+  i=0;j=0
+  }
+  cout<<(x+y);
+  return 0;
+}
