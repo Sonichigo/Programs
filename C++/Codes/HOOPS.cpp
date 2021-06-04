3b3e31d0fd74f2c421b8ee36d3ea386e5d19c12c
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2021-06-04 12:34:47 +0530

    Create HOOPS.cpp

---
+++ C++/Codes/HOOPS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T = 0;
+    cin >> T;
+
+    while(T--)
+    {
+        long long N = 0;
+        cin >> N;
+
+        int t = (N / 2) + 1;
+        cout << t << endl;
+    }
+	return 0;
+}
